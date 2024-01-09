f3286d5ecbc3111fbb09455375fd85f44ec1e382
calebtd <caleb@dillenbeck.org>
2024-01-09 10:29:34 -0700

    basic files, gitignore

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # webdev
+
 This is my personal repo for my web-dev class.
---
+++ createStructure.sh
@@ -0,0 +1,11 @@
+mkdir ~/sandbox/SampleProject
+cd ~/sandbox/SampleProject
+mkdir css pages images js
+touch index.html
+cd /Users/calebdillenbeck/sandbox/SampleProject/css && touch style.css
+cd ~/sandbox/SampleProject/pages && mkdir about contact && cd about && touch about.html about.css
+cd ~/sandbox/SampleProject/pages/contact && touch contact.html contact.css
+cd ~/sandbox/SampleProject/images && touch companyLogo.png && mkdir icons
+cd ~/sandbox/SampleProject/images/icons && touch email.png phone.png chat.png
+cd ~/sandbox/SampleProject/js && touch index.js
+cd ~/sandbox/SampleProject/ && tree
